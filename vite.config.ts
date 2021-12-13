3e9db49aa20895e9bd2b9b7732df901bbb3609a8
zhenyuWang <13641039885@163.com>
2021-12-13 15:14:58 +0800

    chore: development variable add

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,48 @@
-import { defineConfig } from "vite";
+import { UserConfigExport, ConfigEnv, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: "./",
-  resolve: {
-    alias: {
-      "@": resolve("./src"),
+export default ({ mode }: ConfigEnv): UserConfigExport => {
+  const env = loadEnv(mode, process.cwd());
+  // 处理使用import.meta.env jest 测试报错问题
+  const envWithProcessPrefix = Object.entries(env).reduce(
+    (prev, [key, val]) => {
+      return {
+        ...prev,
+        // 环境变量添加process.env
+        ["process.env." + key]: `"${val}"`,
+      };
     },
-  },
-  plugins: [vue()],
-});
+    {}
+  );
+
+  // 生产环境
+  // const proxyTarget = "https://zhgt.yuanzhiyijiantong.com";
+  // 测试环境
+  // const proxyTarget = "http://10.1.69.118:8814";
+  // 仝康本地
+  const proxyTarget = "http://10.2.53.60:8811";
+  return {
+    base: "./",
+    server: {
+      open: true,
+      port: 3000,
+      host: "0.0.0.0",
+      proxy: {
+        "/api": {
+          target: proxyTarget,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    resolve: {
+      alias: {
+        "@": resolve("./src"),
+      },
+    },
+    plugins: [vue()],
+    define: envWithProcessPrefix,
+  };
+};
